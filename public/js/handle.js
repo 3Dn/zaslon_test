107966476ade6bb7ab6a7d04f975e7c482b70884
Krivius <krivitskiy_d@mail.ru>
2015-11-13 14:33:54 +0300

    led-lights change

--- public/js/handle.js
+++ public/js/handle.js
@@ -3,14 +3,13 @@
  */
 $(document).ready(function(){
 
-    $(".cmn-toggle").on("click", function(){
+    $(".cmn-toggle").on("click", function(){ //включаем-выключаем лампочки
         var state = $(this).prop("checked"),
             wrapper = $(this).parents(".box-inner-switches").first();
-        console.log("state: "+state);
         if(state){
-            wrapper.find(".led").addClass("led_on");
+            wrapper.find(".led_light").addClass("led_on");
         }else {
-            wrapper.find(".led").removeClass("led_on")
+            wrapper.find(".led_light").removeClass("led_on")
         }
     });
 
