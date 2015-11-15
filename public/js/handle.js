d68bd05d953240f2cc1a017d17453235717e3258
Kriv <krivitskiy_d@mail.ru>
2015-11-15 16:32:08 +0300

    buttons, jqu-ui interface

--- public/js/handle.js
+++ public/js/handle.js
@@ -3,6 +3,14 @@
  */
 $(document).ready(function(){
 
+    /*$(".draggable").draggable({
+        contaiment: 'parent',
+        cursor: 'move',
+        snap: '#workspace_1'
+    });*/
+
+    $(".u_button").button();
+
     $(".cmn-toggle").on("click", function(){ //включаем-выключаем лампочки
         var state = $(this).prop("checked"),
             wrapper = $(this).parents(".box-inner-switches").first();
@@ -12,6 +20,19 @@ $(document).ready(function(){
             wrapper.find(".led_light").removeClass("led_on")
         }
     });
+
+    $(".u_button").on("click", function(){
+       var ws = $(this).attr("ws");
+        if(ws == '1'){
+            $("#workspace_1").show();
+            $("#workspace_2").hide();
+        }else{
+            $("#workspace_2").show();
+            $("#workspace_1").hide();
+        }
+    });
+
+
 
 });
 
