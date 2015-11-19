a30eab3b7b9f13f0bf8981ae22d73a02e31a2623
Kriv <krivitskiy_d@mail.ru>
2015-11-19 20:32:43 +0300

    setInterval

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -7,16 +7,19 @@ var socket = io.connect();
 socket.on('send', function (data) {
     //console.log(data);
     //socket.emit('recive', { hello: 'world' });
-    var len = data.length,
-        j =data.substring(0, (len-5));
-    var info = {};
-    info = JSON.parse(j);
-    console.log(info);
-    $.each(info.d, function(key, value){
-       if(value == '1'){
-           $("#cmn-toggle-"+key).prop("checked", true);
-           $("#cmn-toggle-"+key).parents(".box-inner-block").find('.led_light').addClass('led_on');
-       }
-    });
+    if(data){
+        var len = data.length,
+            j =data.substring(0, (len-5));
+        var info = {};
+        info = JSON.parse(j);
+        console.log(info);
+        $.each(info.d, function(key, value){
+            if(value == '1'){
+                $("#cmn-toggle-"+key).prop("checked", true);
+                $("#cmn-toggle-"+key).parents(".box-inner-block").find('.led_light').addClass('led_on');
+            }
+        });
+    }
+
 
 });
