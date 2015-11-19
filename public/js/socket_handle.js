5ffcea17c227c899de64a3d6883aba3182d3dce1
Krivius <krivitskiy_d@mail.ru>
2015-11-19 15:55:25 +0300

    toogle switches

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -5,6 +5,17 @@ var socket = io.connect();
 
 
 socket.on('send', function (data) {
-    console.log(data);
-    socket.emit('recive', { hello: 'world' });
+    //console.log(data);
+    //socket.emit('recive', { hello: 'world' });
+    var len = data.length,
+        j =data.substring(0, (len-5));
+    var info = {};
+    info = JSON.parse(j);
+    console.log(info);
+    $.each(info.d, function(key, value){
+       if(value == '1'){
+           $("#cmn-toggle-"+key).prop("checked", true);
+       }
+    });
+
 });
