cdbfabbd78f7217b94436c81050dbae3ce1d8965
Krivius <krivitskiy_d@mail.ru>
2015-11-24 16:30:50 +0300

    test log

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -13,9 +13,11 @@ socket.on('send', function (data) {
         //    j = data.substring(0, pos+2);
         var info = {};
         info = JSON.parse(data);
-        //console.log(info);
+        console.clear();
+        console.log(info);
         $.each(info.t, function(key, value){
-            //console.log("t = "+ value);
+            console.log("t = "+ value);
+
             $("#logs").append("<p> t = "+ value + "</p>");
 
         });
