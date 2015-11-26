644cc893ec6aab8d787cf189e7980ca1ec2806e4
Kriv <krivitskiy_d@mail.ru>
2015-11-26 21:35:56 +0300

    test log

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -7,28 +7,30 @@ var socket = io.connect();
 socket.on('send', function (data) {
     //console.log(data);
     //socket.emit('recive', { hello: 'world' });
-    try{
-        //console.log(data);
-        //var pos = data.lastIndexOf(']}'),
-        //    j = data.substring(0, pos+2);
-        //var info = {};
-        //info = JSON.parse(data);
-        console.log(data);
-        $.each(data.t, function(key, value){
-            console.log("t = "+ value);
+    if(data) {
+        try {
+            //console.log(data);
+            //var pos = data.lastIndexOf(']}'),
+            //    j = data.substring(0, pos+2);
+            //var info = {};
+            //info = JSON.parse(data);
+            console.log(data);
+            $.each(data.t, function (key, value) {
+                console.log("t = " + value);
 
-            $("#logs").append("<p> t = "+ value + "</p>");
+                $("#logs").append("<p> t = " + value + "</p>");
 
-        });
-        $.each(data.d, function(key, value){
-            if(value == '1'){
-                $("#cmn-toggle-"+key).prop("checked", true);
-                $("#cmn-toggle-"+key).parents(".box-inner-block").find('.led_light').addClass('led_on');
-            }
-        });
-    }catch(err){
-        console.log(data);
-        console.log(err.name + ":" + err.message + "\n" + err.stack);
+            });
+            $.each(data.d, function (key, value) {
+                if (value == '1') {
+                    $("#cmn-toggle-" + key).prop("checked", true);
+                    $("#cmn-toggle-" + key).parents(".box-inner-block").find('.led_light').addClass('led_on');
+                }
+            });
+        } catch (err) {
+            console.log(data);
+            console.log(err.name + ":" + err.message + "\n" + err.stack);
+        }
     }
 
 });
