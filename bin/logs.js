0ddb91e20b9da581b447ff9cafadc63b60613b54
Kriv <krivitskiy_d@mail.ru>
2015-11-26 20:28:28 +0300

    test log + module.exports

--- bin/logs.js
+++ bin/logs.js
@@ -1,6 +1,10 @@
 /**
  * Created by Kriv on 26.11.2015.
  */
+var i2c = require('i2c');
+var address = 0x04;
+var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+
 var logs = function logs(){
     //defining a var instead of this (works for variable & function) will create a private definition
 
