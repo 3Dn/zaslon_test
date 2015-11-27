dbe9da36a862d5fa88c996cebe772480b302abe8
Krivius <krivitskiy_d@mail.ru>
2015-11-27 16:43:26 +0300

    pretty scrolls

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -2,10 +2,10 @@
  * Created by Kriv on 16.11.2015.
  */
 var socket = io.connect();
-
-$(document).ready(function(){
-    $("#logs").jScrollPane();
-});
+//
+//$(document).ready(function(){
+//    $("#logs").jScrollPane();
+//});
 
 socket.on('send', function (data) {
     console.log(data);
