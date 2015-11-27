1dfa028c27714a4bc927eee2d2f18dc67237a6d7
3Dn <3Dn@live.ru>
2015-11-27 12:40:57 +0300

    Logs reloaded

--- bin/logs.js
+++ bin/logs.js
@@ -6,14 +6,42 @@ var i2c = require('i2c');
 var address = 0x04;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
-var singleton = function singleton(){
-    //defining a var instead of this (works for variable & function) will create a private definition
+var ret_obj = '';
+
+var wire_loop = setInterval(function(){
     var current_state='';
     var tmp_buff='';
     var obj;
 
+    wire.read(32, function(err, res){
+        var buff_bytes = new Buffer(res);
+        tmp_buff = buff_bytes.toString('utf8');
+        var pos = tmp_buff.lastIndexOf(']}');
+        obj = tmp_buff.substring(0, pos+2);
+    });
+    try {
+        obj = JSON.parse(obj);
+        if (current_state != obj.t[0]) {
+            console.log("curr: " + current_state);
+            current_state = obj.t[0];
+            console.log("new_curr: " + current_state);
+            return ret_obj;
+        }
+    }
+    catch (err) {
+        console.log("obj: " + obj);
+        //console.log(err.name + ":" + err.message + "\n" + err.stack);
+    }
+},5000);
+
+var singleton = function singleton(){
+    //defining a var instead of this (works for variable & function) will create a private definition
+/*    var current_state='';
+    var tmp_buff='';
+    var obj;*/
+
     this.getLogs = function(){
-        wire.read(32, function(err, res){
+ /*       wire.read(32, function(err, res){
             var buff_bytes = new Buffer(res);
             tmp_buff = buff_bytes.toString('utf8');
             var pos = tmp_buff.lastIndexOf(']}');
@@ -31,7 +59,8 @@ var singleton = function singleton(){
             catch (err) {
                 console.log("obj: " + obj);
                 //console.log(err.name + ":" + err.message + "\n" + err.stack);
-            }
+            }*/
+        return ret_obj;
         };
 
 
