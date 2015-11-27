bee7f7fd970b6449a65f41b8f82e903905244cc3
3Dn <3Dn@live.ru>
2015-11-27 11:06:40 +0300

    Logs debug

--- bin/logs.js
+++ bin/logs.js
@@ -6,54 +6,55 @@ var i2c = require('i2c');
 var address = 0x04;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
-var Logs = function(){
+var singleton = function singleton(){
     //defining a var instead of this (works for variable & function) will create a private definition
+    var current_state='';
+    var tmp_buff='';
+    var obj;
 
-    this.current_state='';
-    this.tmp_buff='';
     this.getLogs = function(){
         wire.read(32, function(err, res){
             var buff_bytes = new Buffer(res);
-            this.tmp_buff = buff_bytes.toString('utf8');
-            var pos = this.tmp_buff.lastIndexOf(']}');
-            var obj = this.tmp_buff.substring(0, pos+2);
+            tmp_buff = buff_bytes.toString('utf8');
+            var pos = tmp_buff.lastIndexOf(']}');
+            obj = tmp_buff.substring(0, pos+2);
+        });
             //console.log("Obj before parse: "+obj);
             try {
                 obj = JSON.parse(obj);
                 if (this.current_state != obj.t[0]) {
-                    console.log("curr: "+this.current_state);
+                    console.log("curr: " + current_state);
                     this.current_state = obj.t[0];
                 //    socket.broadcast.emit('send', obj);
-                    console.log("new_curr: "+this.current_state);
+                    console.log("new_curr: " + current_state);
                     //console.log("Obj after parse: "+obj.t[0]);
                     return obj;
                 }
             }
-            catch (err){
-                console.log("obj: "+obj);
+            catch (err) {
+                console.log("obj: " + obj);
                 //console.log(err.name + ":" + err.message + "\n" + err.stack);
             }
-        });
-    };
+        };
 
 
-    if(Logs.caller != Logs.getInstance){
+    if(singleton.caller != singleton.getInstance){
         throw new Error("This object cannot be instanciated");
     }
 };
 
-Logs.instance = null;
+singleton.instance = null;
 
 /**
  * Singleton getInstance definition
  * @return logs class
  */
-Logs.getInstance = function(){
+singleton.getInstance = function(){
     if(this.instance === null){
-        this.instance = new Logs();
+        this.instance = new singleton();
     }
     return this.instance;
 };
 
-module.exports = Logs.getInstance();
+module.exports = singleton.getInstance();
 
