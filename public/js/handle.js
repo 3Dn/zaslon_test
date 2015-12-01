5d301d4fd700c760e293c2626c051c02e0a77dd2
Kriv <krivitskiy_d@mail.ru>
2015-12-01 20:42:38 +0300

    chart.js

--- public/js/handle.js
+++ public/js/handle.js
@@ -38,8 +38,10 @@ $(document).ready(function(){
         }
     });
 
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myLineChart = new Chart(ctx).Line(data, options);
 
-    var chartData = {
+    var data = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
             {
@@ -64,62 +66,57 @@ $(document).ready(function(){
             }
         ]
     };
-    //var ctx = document.getElementById("myChart").getContext("2d");
-    //var myLineChart = new Chart(ctx).Line(chartData);
 
+    var options = {
 
-    Chart.defaults.global = {
-        animation: true,
-        animationSteps: 60,
-        animationEasing: "easeOutQuart",
-        showScale: true,
-        scaleOverride: false,
-        scaleSteps: null,
-        scaleStepWidth: null,
-        scaleStartValue: null,
-        scaleLineColor: "rgba(0,0,0,.1)",
-        scaleLineWidth: 1,
-        scaleShowLabels: true,
-        scaleLabel: "<%=value%>",
-        scaleIntegersOnly: true,
-        scaleBeginAtZero: false,
-        scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-        scaleFontSize: 12,
-        scaleFontStyle: "normal",
-        scaleFontColor: "#666",
-        responsive: false,
-        maintainAspectRatio: true,
-        showTooltips: true,
-        customTooltips: false,
-        tooltipEvents: ["mousemove", "touchstart", "touchmove"],
-        tooltipFillColor: "rgba(0,0,0,0.8)",
-        tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-        tooltipFontSize: 14,
-        tooltipFontStyle: "normal",
-        tooltipFontColor: "#fff",
-        tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-        tooltipTitleFontSize: 14,
-        tooltipTitleFontStyle: "bold",
-        tooltipTitleFontColor: "#fff",
-        tooltipYPadding: 6,
-        tooltipXPadding: 6,
-        tooltipCaretSize: 8,
-        tooltipCornerRadius: 6,
-        tooltipXOffset: 10,
-        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-        multiTooltipTemplate: "<%= value %>",
-        onAnimationProgress: function(){},
-        onAnimationComplete: function(){}
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
+
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+
+        //Boolean - Whether the line is curved between points
+        bezierCurve : true,
+
+        //Number - Tension of the bezier curve between points
+        bezierCurveTension : 0.4,
+
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
+
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 4,
+
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 1,
+
+        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius : 20,
+
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
+
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 2,
+
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : true,
+
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
     };
 
 
-
-    //var ctx = $("#myChart").getContext("2d");
-    //var myNewChart = new Chart(ctx).Line(chartData);
-
-    var canvas = document.getElementById("myChart");
-    var ctx = canvas.getContext("2d");
-    var myNewChart = new Chart(ctx).Line(chartData);
 
 
 
