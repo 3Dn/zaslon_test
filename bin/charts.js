a7af5193462f1d812ceeb7993ec48995a2ecb98f
3Dn <3Dn@live.ru>
2015-12-02 17:26:12 +0300

    Test connection concept

--- bin/charts.js
+++ bin/charts.js
@@ -4,6 +4,15 @@
  */
 
 var db  = require('./db_engine');
+var local_conn = db.connection();
 
+/*
 var db_data = db.query("SELECT * FROM io_log LIMIT 10");
-console.log("db_chart_data: "+db_data);
+console.log("db_chart_data: "+db_data);*/
+
+local_conn.query("SELECT * FROM io_log LIMIT 10", function(err, rows, fields){
+    if (!err)
+        console.log("DB ret: ", rows);
+    else
+        console.log("DB ret error!");
+});
