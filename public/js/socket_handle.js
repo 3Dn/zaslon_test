f0c6f19beb8427ff21c3e44a9c1931812b94fac7
Kriv <krivitskiy_d@mail.ru>
2015-12-08 21:18:02 +0300

    chart.js

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -79,16 +79,7 @@ function init() {
     var chart_data = {
         labels: dates,
         datasets:
-            //{
-            //    label: "Pin states",
-            //    fillColor: "rgba(220,220,220,0.2)",
-            //    strokeColor: "rgba(220,220,220,1)",
-            //    pointColor: "rgba(220,220,220,1)",
-            //    pointStrokeColor: "#fff",
-            //    pointHighlightFill: "#fff",
-            //    pointHighlightStroke: "rgba(220,220,220,1)",
-            //    data: states
-            //},
+        [
             {
                 label: "Pin states",
                 fillColor: "rgba(220,220,220,0.2)",
@@ -100,7 +91,7 @@ function init() {
                 //data: [65, 59, 80, 81, 56, 55, 40, 47, 97, 21]
                 data: states
             }
-
+        ]
     };
    /* var chart_data = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
