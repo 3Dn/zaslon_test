0eaa1f519741faae08a5b4c81e16c0c4b59ff8e9
Kriv <krivitskiy_d@mail.ru>
2015-12-10 21:01:02 +0300

    chart.js

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -109,44 +109,48 @@ function init() {
     arr_clean();
 }
 */
-function MainViewModel(data) {
-    var self = this;
+$(document).ready(function(){
+    function MainViewModel(data) {
+        var self = this;
 
-    self.lineChartData = ko.observable({
-        labels : ["January","February","March","April","May","June","July"],
-        datasets : [
-            {
-                fillColor : "rgba(151,187,205,0.5)",
-                strokeColor : "rgba(151,187,205,1)",
-                pointColor : "rgba(151,187,205,1)",
-                pointStrokeColor : "#fff",
-                data : [65,59,90,81,56,55,40]
-            }
-        ]
-    });
+        self.lineChartData = ko.observable({
+            labels : ["January","February","March","April","May","June","July"],
+            datasets : [
+                {
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    pointColor : "rgba(151,187,205,1)",
+                    pointStrokeColor : "#fff",
+                    data : [65,59,90,81,56,55,40]
+                }
+            ]
+        });
 
-    socket.on('pushdata', function (data) {
-        self.lineChartData().datasets[0].data.shift();
-        self.lineChartData().datasets[0].data.push(data);
+        socket.on('pushdata', function (data) {
+            self.lineChartData().datasets[0].data.shift();
+            self.lineChartData().datasets[0].data.push(data);
 
-        self.initLine();
-    });
+            self.initLine();
+        });
 
-    self.initLine = function() {
-        var options = {
-            animation : false,
-            scaleOverride : true,
-            scaleSteps : 10,//Number - The number of steps in a hard coded scale
-            scaleStepWidth : 10,//Number - The value jump in the hard coded scale
-            scaleStartValue : 10//Number - The scale starting value
-        };
+        self.initLine = function() {
+            var options = {
+                animation : false,
+                scaleOverride : true,
+                scaleSteps : 10,//Number - The number of steps in a hard coded scale
+                scaleStepWidth : 10,//Number - The value jump in the hard coded scale
+                scaleStartValue : 10//Number - The scale starting value
+            };
 
-        var ctx = $("#myChart").get(0).getContext("2d");
-        var myLine = new Chart(ctx).Line( vm.lineChartData(), options );
+            var ctx = $("#myChart").get(0).getContext("2d");
+            var myLine = new Chart(ctx).Line( vm.lineChartData(), options );
+        }
+
     }
+    var vm = new MainViewModel();
+    ko.applyBindings(vm);
+});
 
-}
-var vm = new MainViewModel();
-ko.applyBindings(vm);
+
 
 
