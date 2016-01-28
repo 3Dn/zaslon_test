e25083a0b42d89d41cbfe70db2222efd824dfe5a
3Dn <3Dn@live.ru>
2016-01-28 11:34:59 +0300

    socket

--- public/js/logs_page_scripts.js
+++ public/js/logs_page_scripts.js
@@ -6,13 +6,12 @@ strings = {
     'userSplit': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] отключился.[/sys]'
 };
 window.onload = function() {
-    socket = io();
-    socket.on('connect', function () {
+   // socket.on('connect', function () {
         socket.on('system', function (msg) {
             // Добавляем в лог сообщение, заменив время, имя и текст на полученные
             document.querySelector('#logs').innerHTML += strings[msg.event].replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
             // Прокручиваем лог в конец
-            document.querySelector('#logs').scrollTop = document.querySelector('#log').scrollHeight;
-        });
+            //document.querySelector('#logs').scrollTop = document.querySelector('#log').scrollHeight;
+    //    });
     });
 };
