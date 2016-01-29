9553d02860cdfab70bb3cd35cae8f7ca2ddbb9a1
3Dn <3Dn@live.ru>
2016-01-29 14:11:25 +0300

    dailyLogs

--- public/js/home_page_scripts.js
+++ public/js/home_page_scripts.js
@@ -1,6 +1,4 @@
-window.onload = function() {
-    socket.on('scale_1_log_hour', function(data){
-        document.querySelector('#sacle_1_kgh').innerHTML = data;
-        console.log("Data: " + data);
-    });
-};
+socket.on('scale_1_log_hour', function(data){
+    document.querySelector('#sacle_1_kgh').innerHTML = data;
+    console.log("Data: " + data);
+});
