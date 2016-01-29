8dc2296bfafac12c681797f9d88f35978de5d58e
3Dn <3Dn@live.ru>
2016-01-29 14:53:17 +0300

    dailyLogs

--- public/js/home_page_scripts.js
+++ public/js/home_page_scripts.js
@@ -2,6 +2,6 @@ socket.on('scale_log_home_page', function(data){
     document.querySelector('#sacle_1_kgh').innerHTML = "Кг/ч: " + data.scale_1_hour;
     document.querySelector('#sacle_1_kgd').innerHTML = "Кг/д: " + data.scale_1_day;
     document.querySelector('#sacle_2_kgh').innerHTML = "Кг/ч: " + data.scale_2_hour;
-/*    document.querySelector('#sacle_2_kgd').innerHTML = "Кг/д: " + data.scale_2_day;*/
+    document.querySelector('#sacle_2_kgd').innerHTML = "Кг/д: " + data.scale_2_day;
     console.log("scale_log_home_page: " + data.scale_1_day);
 });
