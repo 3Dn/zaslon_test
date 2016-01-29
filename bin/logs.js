c22f60be8ec0fa192d044093f8da59694740d3a9
3Dn <3Dn@live.ru>
2016-01-29 12:44:11 +0300

    dailyLogs

--- bin/logs.js
+++ bin/logs.js
@@ -83,10 +83,9 @@ var singleton = function singleton(){
                         scale_1_log_hour_count += 40;
                     }
                 });
-                console.log(scale_1_log_hour_count);
             }
+            return scale_1_log_hour_count;
         });
-        return scale_1_log_hour_count;
     };
 
     if(singleton.caller != singleton.getInstance){
