efd521c8aa7e4ffe13700277597b8d1cc609278c
3Dn <3Dn@live.ru>
2016-01-29 14:25:48 +0300

    dailyLogs

--- public/js/home_page_scripts.js
+++ public/js/home_page_scripts.js
@@ -1,4 +1,4 @@
 socket.on('scale_1_log_hour', function(data){
-    document.querySelector('#sacle_1_kgh').innerHTML = 'Кг/ч: '.data;
+    document.querySelector('#sacle_1_kgh').innerHTML = "Кг/ч: " + data;
     console.log("Data: " + data);
 });
