a775d94a98e7255628452e135494d78262d1f9b9
3Dn <3Dn@live.ru>
2016-01-29 12:56:28 +0300

    dailyLogs

--- bin/logs.js
+++ bin/logs.js
@@ -13,10 +13,7 @@ var ret_daily_arr = [];
 var obj = '';
 var current_state='';
 var state_1 = '', state_2 = '';
-var scale_1_log_hour_count = '',
-    scale_2_log_hour_count,
-    scale_1_log_day_count0,
-    scale_2_log_day_count;
+var scale_1_log_hour_count = 0;
 var tmp_buff='';
 
 db.sys_log_query("0", "sys", "0", "System start at: " + Date().toLocaleString());
@@ -85,6 +82,7 @@ var singleton = function singleton(){
                 });
                 scale_1_log_hour_count = ret_count;
                 return scale_1_log_hour_count;
+                console.log(scale_1_log_hour_count);
             }
         });
     };
