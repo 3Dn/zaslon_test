55d5598dff55cc9f25820779fcda3a42ab455ede
Treha <3Dn@live.ru>
2016-02-07 16:56:51 +0300

    logs_page_scripts -> is_loaded variable added

--- public/js/logs_page_scripts.js
+++ public/js/logs_page_scripts.js
@@ -1,3 +1,5 @@
+var is_loaded = 0;
+
 strings = {
     'connected': '[sys][time]%time%[/time]: Вы успешно соединились к сервером как [user]%name%[/user].[/sys]',
     'userJoined': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] вошел.[/sys]',
@@ -8,15 +10,18 @@ strings = {
     'scale_close': '[sys][time]%time%[/time]: Заслонка весов [user]%name%[/user] закрыта.[/sys]'
 };
 window.onload = function() {
-    //socket.on('connect', function () {
+    if(is_loaded == 0) {
+        //socket.on('connect', function () {
         socket.on('sys_to_web', function (msg) {
             // Добавляем в лог сообщение, заменив время, имя и текст на полученные
             document.querySelector('#logs').innerHTML += strings[msg.event].replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
             // Прокручиваем лог в конец
             //document.querySelector('#logs').scrollTop = document.querySelector('#log').scrollHeight;
         });
-        socket.on('scale_log', function(data){
-            data.forEach(function(item, i, data){
+        is_loaded = 1;
+    }
+        socket.on('scale_log', function (data) {
+            data.forEach(function (item, i, data) {
                 var open_close = ['закрыта', 'открыта'];
                 var ltzDate = new Date(item.date.toLocaleString());
                 document.querySelector('#logs').innerHTML += ltzDate + " Контроллер: " + item.name + ". Заслонка #1: " + open_close[item.state_1] + ". Залонка #2: " + open_close[item.state_2] + '.<br>';
