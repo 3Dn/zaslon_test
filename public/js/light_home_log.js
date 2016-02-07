05cda0286448e72d3e9de2b3f9d949bc428d84d9
Treha <3Dn@live.ru>
2016-02-07 19:01:01 +0300

    light_home_log

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -2,7 +2,7 @@
  * Created by 3Dn on 07.02.2016.
  */
 window.onload = function () {
-    socket.on('new_user', function(msg){
+    socket.on('sys_to_ligth_home_log', function(msg){
         document.querySelector('#users_count').innerHTML = "Connected users: " + msg.users_count;
     })
 }
