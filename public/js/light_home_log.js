7337adca7c9702eabf469bbd67b519c983693bcf
Treha <3Dn@live.ru>
2016-02-07 19:07:56 +0300

    light_home_log

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -3,6 +3,6 @@
  */
 window.onload = function () {
     socket.on('sys_to_ligth_home_log', function(msg){
-        document.querySelector('#users_count').innerHTML = "Connected users: " + msg.users_count;
+        document.querySelector('#users_count').innerHTML += "Connected users: " + msg.users_count;
     })
 }
