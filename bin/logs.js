0397336da5c6b94851215c3fd363cd7808b2e7e0
Kriv <krivitskiy_d@mail.ru>
2016-02-07 18:44:58 +0300

    datepicker

--- bin/logs.js
+++ bin/logs.js
@@ -162,9 +162,7 @@ var singleton = function singleton(){
         //var arr_1 = [],
         //    arr_2 = [];
         //var res = {};
-        ret_chart_data = {};
-        arr_1 = [];
-        arr_2 = [];
+
         var sql_1 = "select CONCAT_WS('-',EXTRACT(DAY from date),EXTRACT(MONTH from date), EXTRACT(YEAR from date))as date, count(*) as count"+
             " from scale1_log where date between '"+from+" 00:00:00' and '"+to+" 23:59:59' and state='1' GROUP BY date(DATE)";
         var sql_2 = "select CONCAT_WS('-',EXTRACT(DAY from date),EXTRACT(MONTH from date), EXTRACT(YEAR from date))as date, count(*) as count"+
@@ -201,6 +199,9 @@ var singleton = function singleton(){
         console.log(ret_chart_data);
         console.log("+++++++++++++++++++++++");
         return ret_chart_data;
+        ret_chart_data = {};
+        arr_1 = [];
+        arr_2 = [];
     };
 
     if(singleton.caller != singleton.getInstance){
