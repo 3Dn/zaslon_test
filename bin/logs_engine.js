42415d4127ff0583ff00fe70fb38c9861cb246cd
Treha <3Dn@live.ru>
2016-02-07 16:27:53 +0300

    logs_engine

--- bin/logs_engine.js
+++ bin/logs_engine.js
@@ -1,7 +1,7 @@
 var db  = require('./db_engine');
 var local_logs_conn = db.connection();
 
-function log_write(system, message, level, zaslon_id){
+exports.log_write = function(system, message, level, zaslon_id) {
     if (level == ''){
         level = 0;
     }
@@ -9,12 +9,15 @@ function log_write(system, message, level, zaslon_id){
         zaslon_id = 0;
     }
     local_logs_conn.query('INSERT INTO sys_log(zaslon_id, sys_name, level, message) values("' + zaslon_id + '","' + system + '","' + level + '","' + message + '")');
-}
+};
 
-function log_read(system){
+exports.log_read = function(system) {
     if (system == ''){
         local_logs_conn.query('SELECT * from sys_log');
     } else {
         local_logs_conn.query('SELECT * from sys_log WHERE sys_name="' + system + '"');
     }
-}
+};
+
+module.exports = "logs";
+
