afab34dd61a97c46a79b3862a38106d58f1a65df
3Dn <3Dn@live.ru>
2016-02-08 15:23:00 +0300

    light logs

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -2,7 +2,6 @@
  * Created by 3Dn on 07.02.2016.
  */
 
-window.onload = function () {
     socket.on('sys_to_ligth_home_log_users_count', function(msg){
         document.querySelector('#users_count_inner_text').innerHTML = "Connected users: " + msg.users_count;
     });
@@ -10,4 +9,3 @@ window.onload = function () {
     socket.on('r', function(msg){
         console.log(msg.reconnect);
     });
-};
