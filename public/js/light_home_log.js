7f6151402678c055a05055ac769b8702e3541ac4
3Dn <3Dn@live.ru>
2016-02-08 15:10:51 +0300

    light logs

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -6,7 +6,8 @@ window.onload = function () {
     socket.on('sys_to_ligth_home_log_users_count', function(msg){
         document.querySelector('#users_count_inner_text').innerHTML = "Connected users: " + msg.users_count;
     });
-    socket.on('reconnect', function(msg){
+    socket.emit('ready', {'data': '1'});
+    socket.on('r', function(msg){
         console.log(msg.reconnect);
 
     });
