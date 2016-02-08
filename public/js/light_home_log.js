9723c6829c9c790d796a7db125a3ce27d34eac18
3Dn <3Dn@live.ru>
2016-02-08 12:00:37 +0300

    light logs

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -3,7 +3,7 @@
  */
 window.onload = function () {
     socket.on('sys_to_ligth_home_log_users_count', function(msg){
-        document.querySelector('#users_count').innerHTML = '<p id="user_count">Connected users: ' + msg.users_count + '</p>';
+        document.querySelector('#users_count_inner_text').innerHTML = "Connected users: " + msg.users_count;
     });
     //socket.on()
 };
