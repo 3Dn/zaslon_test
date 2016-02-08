cc95ccec58e2657b98bb06b7a95831c2a1bab2bb
3Dn <3Dn@live.ru>
2016-02-08 10:47:44 +0300

    small fix and update socket.io

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -2,7 +2,8 @@
  * Created by 3Dn on 07.02.2016.
  */
 window.onload = function () {
-    socket.on('sys_to_ligth_home_log', function(msg){
+    socket.on('sys_to_ligth_home_log_users_count', function(msg){
         document.querySelector('#users_count').innerHTML = "Connected users: " + msg.users_count;
-    })
-}
+    });
+    socket.on()
+};
