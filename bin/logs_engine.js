42a2f6e4b3c2e0a8bca95298ae6a74ea97ff39a9
3Dn <3Dn@live.ru>
2016-02-08 17:08:11 +0300

    ligth home log

--- bin/logs_engine.js
+++ bin/logs_engine.js
@@ -12,6 +12,7 @@ exports.log_write = function(system, message, level, zaslon_id) {
 };
 
 exports.log_read = function(system, limit) {
+    var ret_arr = [];
     var sql = '';
     if((system == '') && (limit == '')) {
         sql = 'SELECT * from sys_log';
@@ -23,7 +24,18 @@ exports.log_read = function(system, limit) {
         sql = 'SELECT * from sys_log WHERE sys_name="' + system + '" limit ' + limit;
     }
 
-    //return local_logs_conn.query(sql);
+    local_logs_conn.query(sql, function(err ,rows, fields){
+       rows.forEach(function(item){
+           var obj ={};
+           obj.date = item.date;
+           obj.sys_name = item.sys_name;
+           obj.level = item.level;
+           obj.message = item.message;
+           ret_arr.push(obj);
+       })
+    });
+
+    return ret_arr;
 
 /*
     if (system == ''){
