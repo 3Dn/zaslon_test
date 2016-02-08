e40390674c02047abb71d6a269512f6d2ddd4c57
3Dn <3Dn@live.ru>
2016-02-08 16:05:40 +0300

    ligth home log

--- bin/logs_engine.js
+++ bin/logs_engine.js
@@ -11,12 +11,32 @@ exports.log_write = function(system, message, level, zaslon_id) {
     local_logs_conn.query('INSERT INTO sys_log(zaslon_id, sys_name, level, message) values("' + zaslon_id + '","' + system + '","' + level + '","' + message + '")');
 };
 
-exports.log_read = function(system) {
+exports.log_read = function(system, limit) {
+    var sql = '';
+    if((system == '') && (limit == '')) {
+        sql = 'SELECT * from sys_log';
+    } else if ((system != '') && (limit == '')) {
+        sql = 'SELECT * from sys_log WHERE sys_name="' + system + '"';
+    } else if ((system == '') && (limit != '')) {
+        sql = 'SELECT * from sys_log limit ' + limit;
+    } else if ((system != '') && (limit != '')) {
+        sql = 'SELECT * from sys_log WHERE sys_name="' + system + '" limit ' + limit;
+    }
+
+    local_logs_conn.query(sql);
+
+/*
     if (system == ''){
         local_logs_conn.query('SELECT * from sys_log');
     } else {
         local_logs_conn.query('SELECT * from sys_log WHERE sys_name="' + system + '"');
     }
+
+    if(limit == '') {
+        sql = 'SELECT * from sys_log';
+    } else {
+        sql = 'SELECT * from sys_log LIMIT ' + limit;
+    }*/
 };
 
 module.exports.logs = "logs";
--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -2,8 +2,11 @@
  * Created by 3Dn on 07.02.2016.
  */
 
-    socket.on('sys_to_ligth_home_log_users_count', function(msg){
-        document.querySelector('#users_count_inner_text').innerHTML = "Connected users: " + msg.users_count;
+    socket.on('sys_to_ligth_home_log_users_count', function(data){
+        document.querySelector('#users_count_inner_text').innerHTML = "Connected users: " + data.users_count;
+    });
+    socket.on('sys_to_ligth_home_log_last_logs', function(data){
+        document.querySelector('#last_logs_inner_text').innerHTML += data.text;
     });
 /*
     socket.emit('ready', {'data': '1'});
