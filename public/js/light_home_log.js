81852a111d6bf14a9590e29e1d5f0fb72e8f07ef
3Dn <3Dn@live.ru>
2016-02-08 17:21:06 +0300

    ligth home log

--- public/js/light_home_log.js
+++ public/js/light_home_log.js
@@ -6,11 +6,15 @@
         document.querySelector('#users_count_inner_text').innerHTML = "Connected users: " + data.users_count;
     });
     socket.on('sys_to_ligth_home_log_last_logs', function(data){
+
         document.querySelector('#last_logs_inner_text').innerHTML += data.text;
     });
     socket.emit('ready', {'data': '1'});
     socket.on('r', function(msg){
         console.log(msg.reconnect);
+        msg.last_news.forEach(function(item){
+            document.querySelector('#last_logs_inner_text').innerHTML += item.message +'<br>';
+        });
         console.log(msg.last_news[0]);
         console.log(msg.last_news.length);
     });
