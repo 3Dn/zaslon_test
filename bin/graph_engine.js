89396fb79b25ad366eec0cdb0ff2ee4260a0da0e
3Dn <3Dn@live.ru>
2016-02-16 16:57:47 +0300

    test graphs

--- bin/graph_engine.js
+++ bin/graph_engine.js
@@ -2,18 +2,19 @@
  * Created by adm_korolev on 16.02.2016.
  */
 var db  = require('./db_engine');
-var local_logs_conn = db.connection();
+var local_conn = db.connection();
 
 exports.kgph = function(base){
-    var t_arr = [];
     var ret_arr = [];
+    var t_arr = [];
     var sql = 'SELECT state, date from ' + base + ' WHERE state=0 and date >= DATE_SUB(NOW(), INTERVAL 1 DAY)';
-    local_logs_conn.query(sql, function(err, rows, fields){
-        rows.forEach(function(item){
+    local_conn.query(sql, function(err, rows, fields) {
+        rows.forEach(function (item) {
             var obj = {};
             obj.date = item.date;
             t_arr.push(obj.date);
         });
+
         for(var i = 0; i < t_arr.length; i=i+2 ){
             var t_obj = {};
             t_obj.date = t_arr[i];
@@ -23,10 +24,10 @@ exports.kgph = function(base){
             console.log("Graph_engine state  -> " + t_obj.state);*/
             ret_arr.push(t_obj);
         }
-        console.log("Graph_engine -> " + ret_arr);
-        return ret_arr;
+        //console.log("Graph_engine 1 -> " + ret_arr);
     });
-    console.log("Graph_engine -> " + ret_arr);
+    //console.log("Graph_engine 2 -> " + ret_arr);
+    return ret_arr;
 };
 
 module.exports.graph_engine = "graph_engine";
