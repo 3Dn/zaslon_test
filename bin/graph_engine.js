94235b333fa8b06dd99628b28384dbb9c843c4dc
3Dn <3Dn@live.ru>
2016-02-16 16:03:26 +0300

    test graphs

--- bin/graph_engine.js
+++ bin/graph_engine.js
@@ -15,17 +15,17 @@ exports.kgph = function(base){
             t_arr.push(obj.date);
         });
         console.log("Graph_engine l-> " + t_arr.length);
+        for(var i = 0; i < t_arr.length; i=i+2 ){
+            var t_obj = {};
+            t_obj.date = t_arr[i];
+            t_obj.state = t_arr[i]-t_arr[i+1];
+            console.log("Graph_engine date-> " + t_obj.date);
+            console.log("Graph_engine state-> " + t_obj.state);
+            ret_arr.push(t_obj);
+        }
+        console.log("Graph_engine -> " + ret_arr);
+        return ret_arr;
     });
-    for(var i = 0; i < t_arr.length; i=i+2 ){
-        var t_obj = {};
-        t_obj.date = t_arr[i];
-        t_obj.state = t_arr[i]-t_arr[i+1];
-        console.log("Graph_engine date-> " + t_obj.date);
-        console.log("Graph_engine state-> " + t_obj.state);
-        ret_arr.push(t_obj);
-    }
-    console.log("Graph_engine -> " + ret_arr);
-    return ret_arr;
 };
 
 module.exports.graph_engine = "graph_engine";
