775512779583db5624c99bdbe514f00db87fc1ce
3Dn <3Dn@live.ru>
2016-02-16 15:48:47 +0300

    test graphs

--- bin/graph_engine.js
+++ bin/graph_engine.js
@@ -14,13 +14,13 @@ exports.kgph = function(base){
             obj.date = item.date;
             t_arr.push(obj.date);
         });
-        for(var i = 0; i < t_arr.length; i=i+2 ){
-            var t_obj = {};
-            t_obj.date = t_arr[i];
-            t_obj.state = t_arr[i]-t_arr[i+1];
-            ret_arr.push(t_obj);
-        }
     });
+    for(var i = 0; i < t_arr.length; i=i+2 ){
+        var t_obj = {};
+        t_obj.date = t_arr[i];
+        t_obj.state = t_arr[i]-t_arr[i+1];
+        ret_arr.push(t_obj);
+    }
     console.log(ret_arr);
     return ret_arr;
 };
