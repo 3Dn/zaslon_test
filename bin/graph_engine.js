f2c835bcc51567cf7c5baa6d77aed4913a15849b
3Dn <3Dn@live.ru>
2016-02-16 16:17:29 +0300

    test graphs

--- bin/graph_engine.js
+++ bin/graph_engine.js
@@ -17,8 +17,8 @@ exports.kgph = function(base){
         for(var i = 0; i < t_arr.length; i=i+2 ){
             var t_obj = {};
             t_obj.date = t_arr[i];
-            var date_parts = t_arr[i].split(" ");
-            var firstDate = new Date(date_parts[1], date_parts[2], date_parts[3], date_parts[4]);
+            //var date_parts = t_arr[i].split(" ");
+            //var firstDate = new Date(date_parts[1], date_parts[2], date_parts[3], date_parts[4]);
             console.log("Graph_engine state-> " + firstDate);
            // var secondDate =
             t_obj.state = t_arr[i]-t_arr[i+1];
