c0dacdd6751616bc2346c1f5dbe322af73ecbd1e
3Dn <3Dn@live.ru>
2016-03-01 16:49:03 +0300

    test graphs

--- bin/graph_engine.js
+++ bin/graph_engine.js
@@ -2,45 +2,32 @@
  * Created by adm_korolev on 16.02.2016.
  */
 var db  = require('./db_engine');
+var dateFormat  = require('dateformat');
 var local_conn = db.connection();
 
-var rez_return = [];
-
-exports.killogram_per_hour = function(str){
-
+function killogram_per_hour(db, callback) {
     var ret_arr = [];
     var t_arr = [];
-    var sql = 'SELECT state, date from ' + str + ' WHERE state=0 and date >= DATE_SUB(NOW(), INTERVAL 3 DAY)';
-    local_conn.query(sql, function(err, rows, fields) {
+    var sql = 'SELECT state, date from ' + db + ' WHERE state=0 and date >= DATE_SUB(NOW(), INTERVAL 3 DAY)';
+    local_conn.query(sql, function (err, rows, fields) {
+        if (err) throw err;
         rows.forEach(function (item) {
-            var obj = {};
-            obj.date = item.date;
-            t_arr.push(obj.date);
+            t_arr.push(item.date);
         });
+        //console.log('Test: ', dateFormat(rows[i].date, "dd-mm-yyyy HH:MM:ss"));
 
         for(var i = 0; i < t_arr.length; i=i+2 ){
             var t_obj = {};
             var arr = [];
-            var temp_date = new Date(t_arr[i]);
-
-            //console.log(temp_date.getDay() + "." + temp_date.getMonth() + "." + temp_date.getFullYear() + " / " + temp_date.getHours() + ":" + temp_date.getMinutes());
-            //t_obj.date = temp_date;
-
-            t_obj.date = Date.UTC(temp_date.getFullYear(), temp_date.getMonth(), temp_date.getDay(), temp_date.getHours(), temp_date.getMinutes());
-
+            t_obj.date = Date.UTC(dateFormat(t_arr[i], "yyyy"), dateFormat(t_arr[i], "m"), dateFormat(t_arr[i], "dd"), dateFormat(t_arr[i], "HH"), dateFormat(t_arr[i], "MM"), dateFormat(t_arr[i], "ss"));
             var delta = (t_arr[i+1]-t_arr[i])/1000;
             var per_seconds = (20 / delta)*3600;
             t_obj.state = per_seconds - (per_seconds%1); // (20 / dt) * 3600 <- (== 60*60) in hour
-
             arr = [t_obj.date, t_obj.state];
-/*            console.log("Graph_engine date_1 -> " + t_arr[i]);
-            console.log("Graph_engine date_2 -> " + t_arr[i+1]);
-            console.log("Graph_engine state  -> " + t_obj.state);*/
             ret_arr.push(arr);
         }
-        rez_return = ret_arr;
+        callback(ret_arr);
     });
-    return rez_return;
-};
+}
 
-module.exports.graph_engine = "graph_engine";
+module.exports.killogram_per_hour = killogram_per_hour;
