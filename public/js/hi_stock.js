1a54d9e0404a8bcf2606f1d5764f3e171b9326b3
3Dn <3Dn@live.ru>
2016-03-02 16:37:02 +0300

    test graphs

--- public/js/hi_stock.js
+++ public/js/hi_stock.js
@@ -37,6 +37,27 @@ $(function () {
                         [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
                     ]
                 }
+            },
+                {
+                name : '2.5',
+                data : d.all_history,
+                type : 'areaspline',
+                threshold : null,
+                tooltip : {
+                    valueDecimals : 2
+                },
+                fillColor : {
+                    linearGradient : {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops : [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                }
             }]
         });
     });
