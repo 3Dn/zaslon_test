a8fe0c696b82c9fc74e162f4e2e027f2f5d15ba0
3Dn <3Dn@live.ru>
2016-03-02 16:14:09 +0300

    test graphs

--- bin/graph_engine.js
+++ bin/graph_engine.js
@@ -41,7 +41,7 @@ function all_history(db, callback) {
             var t_arr = [];
             var t_obj_date = Date.UTC(dateFormat(item.date, "yyyy"), dateFormat(item.date, "m"), dateFormat(item.date, "dd"));
             t_arr = [t_obj_date, item.count];
-            arr.push(arr);
+            arr.push(t_arr);
         });
         callback(arr);
     });
