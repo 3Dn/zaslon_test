f4748d27223f5812b2f8a4e18b187b34ee48f274
Kriv <krivitskiy_d@mail.ru>
2016-03-24 14:12:43 +0300

    highstock datepicker

--- public/js/hi_stock.js
+++ public/js/hi_stock.js
@@ -17,8 +17,8 @@ $(function () {
                 text : 'Данные по дням'
             },
             rangeSelector: {
-                allButtonsEnabled: true,
-                selected: 2
+                //allButtonsEnabled: true,
+                selected: 0
 
             },
 
@@ -72,21 +72,21 @@ $(function () {
     });
 
     $.datepicker.setDefaults({
-        //changeMonth: true,
-        //numberOfMonths: 1,
-        //dateFormat: "dd-mm-yy",
-        //maxDate: new Date(),
-        //closeText: 'Закрыть',
-        //prevText: '&#x3c;Пред',
-        //nextText: 'След&#x3e;',
-        //currentText: 'Сегодня',
-        //monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
-        //    'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-        //monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
-        //    'Июл','Авг','Сен','Окт','Ноя','Дек'],
-        //dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-        //dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-        //dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        changeMonth: true,
+        numberOfMonths: 1,
+        dateFormat: "dd-mm-yy",
+        maxDate: new Date(),
+        closeText: 'Закрыть',
+        prevText: '&#x3c;Пред',
+        nextText: 'След&#x3e;',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+            'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+            'Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
         onSelect: function () {
             this.onchange();
             this.onblur();
