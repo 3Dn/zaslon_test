0f99b6cbfdb37e01320c888867a056554e2b9bea
kriv <krivitskiy_d@mail.ru>
2016-04-04 13:15:08 +0300

    templates

--- public/js/handle.js
+++ public/js/handle.js
@@ -280,7 +280,18 @@ $(document).ready(function(){
     });
 */
 
-
+    $("#show_templates").on("click",  function(){
+        var state = $(this).attr("state");
+        if(state == '0'){
+            $("#templates").show("slide", { direction: "left" }, 500);
+            $(this).attr("state", '1');
+            $(this).html("&#10097");
+        }else{
+            $("#templates").hide("slide", { direction: "right" }, 500);
+            $(this).attr("state", '0');
+            $(this).html("&#10096");
+        }
+    });
 
 
 });
