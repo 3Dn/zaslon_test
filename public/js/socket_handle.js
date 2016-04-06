6251e59e5f3e5a308164b2c564dd4c55c2bb090e
kriv <krivitskiy_d@mail.ru>
2016-04-06 17:19:03 +0300

    edit template

--- public/js/socket_handle.js
+++ public/js/socket_handle.js
@@ -97,11 +97,27 @@ socket.on("load_template_list", function(data){
     $("#template_list").html(list);
 });
 
-
+//1_1 && 2_2 && 3_3 && 4_4 && 5_5 && 6_6
 socket.on("load_template_info", function(data){
     var info = JSON.parse(data);
-    
-    $("#edit_template_dialog").dialog("option", "title", "Редактирование шаблона &laquo;"+info.name+"&raquo;").dialog("open");
+    var template = info.template;
+    template = template.split("&&");
+    var qwe = '<input type="hidden" value="'+info.id+'" id="template_edit_id">'+
+        '<label for="template_name">Название:</label>'+
+        '<input type="text" value="'+info.name+'" id="template_name" style="width:125px;"><br>'+
+        '<label for="nor_1_low">Нория 1:</label>'+
+        '<input type="text" value="'+templates[0].split("_")[0]+'" id="nor_1_low"> - <input type="text" value="'+templates[0].split("_")[1]+'" id="nor_1_up"><br>'+
+        '<label for="nor_2_low">Нория 2:</label>'+
+        '<input type="text" value="'+templates[1].split("_")[0]+'" id="nor_2_low"> - <input type="text" value="'+templates[1].split("_")[1]+'" id="nor_2_up"><br>'+
+        '<label for="nor_3_low">Нория 3:</label>'+
+        '<input type="text" value="'+templates[2].split("_")[0]+'" id="nor_3_low"> - <input type="text" value="'+templates[2].split("_")[1]+'" id="nor_3_up"><br>'+
+        '<label for="blansh_low">Бланшир:</label>'+
+        '<input type="text" value="'+templates[3].split("_")[0]+'" id="blansh_low"> - <input type="text" value="'+templates[3].split("_")[1]+'" id="blansh_up"><br>'+
+        '<label for="knives_low">Ножи:</label>'+
+        '<input type="text" value="'+templates[4].split("_")[0]+'" id="knives_low"> - <input type="text" value="'+templates[4].split("_")[1]+'" id="knives_up"><br>'+
+        '<label for="vibro_low">Сетки:</label>'+
+        '<input type="text" value="'+templates[5].split("_")[0]+'" id="vibro_low"> - <input type="text" value="'+templates[5].split("_")[1]+'" id="vibro_up">';
+    $("#edit_template_dialog").html(qwe).dialog("option", "title", "Редактирование шаблона "+info.name).dialog("open");
 });
 
 
